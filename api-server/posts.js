4cd4dcc6020875c6f3cc4db14118007eb4a95528
ansarimofid <ansarimofid@gmail.com>
2017-11-08 00:03:18 +0530

    Initial Commit

---
+++ api-server/posts.js
@@ -0,0 +1,151 @@
+const clone = require('clone')
+
+let db = {}
+
+const defaultData = {
+  "8xf0y6ziyjabvozdd253nd": {
+    id: '8xf0y6ziyjabvozdd253nd',
+    timestamp: 1467166872634,
+    title: 'Udacity is the best place to learn React',
+    body: 'Everyone says so after all.',
+    author: 'thingtwo',
+    category: 'react',
+    voteScore: 6,
+    deleted: false,
+    commentCount: 2
+  },
+  "6ni6ok3ym7mf1p33lnez": {
+    id: '6ni6ok3ym7mf1p33lnez',
+    timestamp: 1468479767190,
+    title: 'Learn Redux in 10 minutes!',
+    body: 'Just kidding. It takes more than 10 minutes to learn technology.',
+    author: 'thingone',
+    category: 'redux',
+    voteScore: -5,
+    deleted: false,
+    commentCount: 0
+  },
+  "6ni6ok3ym7mf1p33lnew": {
+    id: '6ni6ok3ym7mf1p33lnew',
+    timestamp: 1509813411000,
+    title: 'Lorem ispum',
+    body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab alias amet aperiam beatae corporis debitis dignissimos dolor dolore dolores ducimus ex facere fugit harum illo illum inventore ipsa labore laboriosam molestiae nam nemo nihil nulla numquam, obcaecati praesentium quasi quo quod reiciendis rem saepe sapiente similique soluta, tempora tempore veniam voluptas! Aperiam cumque distinctio dolores dolorum enim libero maxime, mollitia natus nostrum porro praesentium quam quas quod quos reiciendis, rerum sequi sint sunt tenetur ullam. Cum minima necessitatibus, perspiciatis sit sunt suscipit ullam ut. Corporis dolorum porro possimus quibusdam velit. Distinctio harum molestiae nulla obcaecati quasi saepe velit veritatis.',
+    author: 'mofuu',
+    category: 'redux',
+    voteScore: -5,
+    deleted: false,
+    commentCount: 0
+  }
+}
+
+function getData (token) {
+  let data = db[token]
+  if (data == null) {
+    data = db[token] = clone(defaultData)
+  }
+  return data
+}
+
+function getByCategory (token, category) {
+  return new Promise((res) => {
+    let posts = getData(token)
+    let keys = Object.keys(posts)
+    let filtered_keys = keys.filter(key => posts[key].category === category && !posts[key].deleted)
+    res(filtered_keys.map(key => posts[key]))
+  })
+}
+
+function get (token, id) {
+  return new Promise((res) => {
+    const posts = getData(token)
+    res(
+      posts[id].deleted
+        ? {}
+        : posts[id]
+    )
+  })
+}
+
+function getAll (token) {
+  return new Promise((res) => {
+    const posts = getData(token)
+    let keys = Object.keys(posts)
+    let filtered_keys = keys.filter(key => !posts[key].deleted)
+    res(filtered_keys.map(key => posts[key]))
+  })
+}
+
+function add (token, post) {
+  return new Promise((res) => {
+    let posts = getData(token)
+
+    posts[post.id] = {
+      id: post.id,
+      timestamp: post.timestamp,
+      title: post.title,
+      body: post.body,
+      author: post.author,
+      category: post.category,
+      voteScore: 1,
+      deleted: false,
+      commentCount: 0
+    }
+
+    res(posts[post.id])
+  })
+}
+
+function vote (token, id, option) {
+  return new Promise((res) => {
+    let posts = getData(token);
+    post = posts[id];
+    switch(option) {
+        case "upVote":
+            post.voteScore = post.voteScore + 1;
+            break;
+        case "downVote":
+            post.voteScore = post.voteScore - 1;
+            break;
+        default:
+            console.log(`posts.vote received incorrect parameter: ${option}`)
+    }
+    res(post)
+  })
+}
+
+function disable (token, id) {
+    return new Promise((res) => {
+      let posts = getData(token)
+      posts[id].deleted = true
+      res(posts[id])
+    })
+}
+
+function edit (token, id, post) {
+    return new Promise((res) => {
+        let posts = getData(token)
+        for (prop in post) {
+            posts[id][prop] = post[prop]
+        }
+        res(posts[id])
+    })
+}
+
+function incrementCommentCounter(token, id, count) {
+  const data = getData(token)
+  if (data[id]) {
+    data[id].commentCount += count
+  }
+}
+
+module.exports = {
+  get,
+  getAll,
+  getByCategory,
+  add,
+  vote,
+  disable,
+  edit,
+  getAll,
+  incrementCommentCounter
+}
