8376c00a2b8c3c524f3fb1fae8cc76ba92d05a64
ansarimofid <ansarimofid@gmail.com>
2017-11-08 18:18:42 +0530

    Adds full post page

--- frontend/src/Component/Posts/Posts.js
+++ frontend/src/Component/Posts/Posts.js
@@ -2,30 +2,54 @@
  * Created by ansarimofid on 04/11/17.
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import PostCards from '../PostCard/PostCard'
 import './Posts.css'
-import {loadPost} from '../../Actions/postAction'
+import {loadCategoryPost, loadPost} from '../../Actions/postAction'
 
 import {connect} from 'react-redux'
+import {sortPosts} from "../../utility/sort";
 
 class Posts extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {sort: 'popular'};
+  }
 
   componentWillMount() {
-    this.props.loadPost();
+    if (this.props.category) {
+      this.props.loadPostByCategory(this.props.category);
+    } else {
+      this.props.loadPost();
+    }
   }
 
   renderAllPost() {
-    // console.log('Props', this.props);
-    const {posts} = this.props.posts;
-    return posts.map((post)=>
+    let {posts} = this.props.posts;
+    posts = sortPosts(posts, this.state.sort);
+    return posts.map((post) =>
       <PostCards key={post.id} post={post}/>)
+  }
+
+  getSortButtons() {
+    return (
+      <div>
+        {this.state.sort === 'latest' ?
+          <span>Latest<button onClick={(event) => this.setState({...this.state, sort: 'popular'})}>Popular</button></span> :
+          <span><button onClick={(event) => this.setState({...this.state, sort: 'latest'})}>Latest</button>Popular</span>
+        }
+      </div>
+    )
   }
 
   render() {
     return (
       <div>
-        <h3 className="uk-text-left uk-padding-small posts-title uk-margin-remove-bottom uk-padding-remove-bottom"><span>POSTS</span><hr/></h3>
+        {this.getSortButtons()}
+        <h3 className="uk-text-left uk-padding-small posts-title uk-margin-remove-bottom uk-padding-remove-bottom">
+          <span>POSTS</span>
+          <hr/>
+        </h3>
         <div className="uk-grid">{this.renderAllPost()}</div>
       </div>
     );
@@ -39,5 +63,7 @@ function mapStateToProps(state) {
   }
 }
 
-export default connect(mapStateToProps,{
-  loadPost: loadPost})(Posts);
+export default connect(mapStateToProps, {
+  loadPost: loadPost,
+  loadPostByCategory: loadCategoryPost
+})(Posts);
